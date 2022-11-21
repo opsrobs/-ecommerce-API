e67b4261668a87745655f94941d238adc8b47e8f
Robson Freitas <robson.flavio20@outlook.com>
2022-11-21 18:43:30 -0300

    :p

--- src/main/java/com/ecommerce/api/apiecommerce/ApiEcommerceApplication.java
+++ src/main/java/com/ecommerce/api/apiecommerce/ApiEcommerceApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ApiEcommerceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws RuntimeException {
 		SpringApplication.run(ApiEcommerceApplication.class, args);
 		System.err.println("\n\nVai lá jovem. A API está de pé 👌👌👌");
 	}
