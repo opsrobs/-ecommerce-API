45309e75c7adf8c1d60c2c5a11990631e0e3f3ef
Robson Freitas <robson.flavio20@outlook.com>
2022-11-21 18:51:43 -0300

    Adicionada notação spring @Service que estava pendente

--- src/main/java/com/ecommerce/api/apiecommerce/Services/ClienteServices.java
+++ src/main/java/com/ecommerce/api/apiecommerce/Services/ClienteServices.java
@@ -3,10 +3,11 @@ package com.ecommerce.api.apiecommerce.Services;
 import com.ecommerce.api.apiecommerce.Models.CityModels;
 import com.ecommerce.api.apiecommerce.Models.ClienteModels;
 import com.ecommerce.api.apiecommerce.Repositories.ClienteRepositories;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
-
+@Service
 public class ClienteServices {
     final ClienteRepositories clienteRepositories;
 
