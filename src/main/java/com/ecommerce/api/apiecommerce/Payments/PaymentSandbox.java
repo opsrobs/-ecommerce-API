9b2fb80f60a61abb95c2e8520a163cdfb2b7008f
Robson Freitas <robson.flavio20@outlook.com>
2022-11-28 20:15:55 -0300

    dependencies gradle paypal

--- src/main/java/com/ecommerce/api/apiecommerce/Payments/PaymentSandbox.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.api.apiecommerce.Payments;
-
-public class PaymentSandbox {
-    Credentials cr = new Credentials();
-    private final String EMAIL = cr.email();
-    private final String TOKEN =cr.token();
-
-}
